aa6061936ff387620ecd4a3ca51c1fe06d7d592b
dev-rashidul <rashidul.contact@gmail.com>
2024-04-30 11:41:25 +0600

    Where Go section done

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// JavaScript For Sticky Header
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
