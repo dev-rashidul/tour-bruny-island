28578ab5008f90773bf4d9570240cfd6da09537d
Rashidul Islam <rashidul.contact@gmail.com>
2024-05-04 23:39:03 +0600

    Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,25 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector(".navbar");
   header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+// Landscapes Slider
+
+$(".landscapes__slider").owlCarousel({
+  loop: true,
+  margin: 25,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
